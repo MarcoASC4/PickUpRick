3b2754f8e681fd647a7817a75020f8043308a63b
DonnieASC4 <dsparks18@deerfield.edu>
2017-08-16 09:34:51 -0400

    First ATR with Comments

--- MostUpdatedRick/ATR.js
+++ MostUpdatedRick/ATR.js
@@ -48,28 +48,38 @@ function preload() {
 }
 
 function setup() {
+    // Sets the size of the canvas to 600 by 600. Don't change this size yet because the
+    // whole code is revolved around this size. I need to make a formula to make it better.
     createCanvas(600, 600);
     button = createButton('toggle');
     button.mousePressed(toggleSong);
     song.play();
+    // The .8 is the smoothing function to make the function less bunched up.
     amp = new p5.Amplitude(.8 );
+    // Determines the Y of the graph at the farthest left side of the canvas.
     firstIndexY = 0;
+    // Determines the Y of the graph at the farthest right side of the canvas
     secondIndexY = 0;
+    // These probabilities when true will make Trash or Sharks spawn.
     probabilityTrash = false;
     probabilitySharks = false;
+    // Once the screen is filled with the graph then startSpawners will be true and trash and sharks can start to appear.
     startSpawners = false;
+    // Makes the song 3 times as fast so the level is 3 times as fast.
+    // Not the final level
+    song.rate(3);
 
 }
 
 
+// These numbers may need to be lower
 function P() {
     // This makes the function have really low probability. You might need to change this number.
     var prob = Math.random()*1;
     if (prob < 0.0001) {
         probabilityTrash = true;
-        console.log("I'm true");
     } else if (prob >= 0.0001 && prob < 0.0002) {
-        console.log("No I'm true");
+    
         probabilitySharks = true;
     }
 
@@ -78,17 +88,18 @@ function P() {
 function draw() {
 
     background(0, 119, 190);
+    // gives volume at this instance of the song.
     var vol = amp.getLevel();
     volhistory.push(vol)
     stroke(0);
     push();
-    var currentY = map(vol, 0, 1, height, 0);
-    //translate(0, height / 2 - currentY);
     
     beginShape();
     stroke(0,255,0)
+    // makes 200 vertexes
     for (var i = 0; i < 200; i++) {
         var y = map(volhistory[i], 0, 1, height / 2, 0);
+        // formula to approximate 200 vertexes
         vertex(1 + 3*i, y);
         P();
         if (i === 0) {
@@ -98,12 +109,10 @@ function draw() {
             secondIndexY = y;
             // If probability has been met then a new trash will spawn at the last index (all the way to the right on the canvas).
             if (probabilityTrash === true && startSpawners === true) {
-               // console.log("TrashShow??");
                 SpawnerArray.push({num: i, trash: probabilityTrash, shark: false})
                 probabilityTrash = false;
             }
             if (probabilitySharks === true && startSpawners === true) {
-                //console.log("SharkShow?");
                 SpawnerArray.push({num: i, trash: false, shark: probabilitySharks})
                 probabilitySharks = false;
                 
@@ -158,11 +167,12 @@ function draw() {
         var yVal = map(volhistory[Characterx], 0, 1, height / 2, 0) - 25;
         ypos = yVal;
         rect(1 + Characterx*3 - squareLength / 2, yVal, squareLength, squareLength);
-
+        
     } else {
+        // Makes the red square jump
         if (moveUp===true) {
-            //console.log("Going Up")
-            ypos -= 5;
+            
+            ypos -= 6;
             totalMoveUpDist -= 10;
             if (totalMoveUpDist <= 0) {
                 moveUp = false;
@@ -170,18 +180,19 @@ function draw() {
             }
         }
         if (moveDown===true) {
-            ypos += 5;
+            ypos += 3;
             totalMoveUpDist+=10;
-            if (totalMoveUpDist >= 300) {
+            if (totalMoveUpDist >= 600) {
                 moveDown = false;
                 followSong = true;
+                totalMoveUpDist = 300;
             }
         }
         fill(255,0,0);
         rect(xpos, ypos, 50, 50);
 
     }
-    //console.log(followSong)
+    
 }
 function keyPressed() {
     if (keyCode === UP_ARROW && !moveDown) {
